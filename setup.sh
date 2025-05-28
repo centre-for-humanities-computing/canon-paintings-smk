a049d6661e15daedd6a49f20c40dbb4f101da408
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-05-28 15:06:30 +0200

    update notebooks

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
+# create virtual environment
 python3 -m venv env
 
+# activate env
 source ./env/bin/activate
 
+# install requirements
 pip install -r requirements.txt
 
+# explicitly install ipykernel
 pip install ipykernel
 
-python -m ipykernel install --user --name=env
+# install kernel so env can be used in jupyter notebooks
+python -m ipykernel install --user --name=canon_env
 
 deactivate 
