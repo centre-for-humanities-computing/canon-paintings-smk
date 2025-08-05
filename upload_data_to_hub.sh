021ea68149a63bdc4462b9a7350d992dcc1b1dd0
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-08-05 14:43:44 +0200

    update gitignore + add nb on canon creation

--- upload_data_to_hub.sh
+++ upload_data_to_hub.sh
@@ -4,6 +4,6 @@ pip install huggingface_hub
 
 huggingface-cli login
 
-python3 src/upload_to_hub.py --dataset smk_only_paintings --hub_name smk_only_paintings
+python3 src/upload_to_hub.py --dataset smk_canon_paintings --hub_name smk_canon_paintings
 
 deactivate
