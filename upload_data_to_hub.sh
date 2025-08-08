f84ef444c56735df374bc2cfd2b82555658558bf
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-08-08 14:37:15 +0200

    remove old scripts

--- upload_data_to_hub.sh
+++
@@ -1,9 +0,0 @@
-source ./env/bin/activate
-
-pip install huggingface_hub
-
-huggingface-cli login
-
-python3 src/upload_to_hub.py --dataset smk_canon_paintings --hub_name smk_canon_paintings
-
-deactivate
