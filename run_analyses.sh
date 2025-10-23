af5ae93e76b2538e227492ffc878c32d55eda260
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-10-23 11:30:26 +0200

    updated dataset name

--- run_analyses.sh
+++ run_analyses.sh
@@ -3,6 +3,6 @@ source ./env/bin/activate
 # necessary in order to use utils scripts
 export PYTHONPATH=$(pwd)
 
-python3 src/run_analyses.py --df_name smk_canon_paintings.pkl --ds_name smk_canon_paintings --plot --classification
+python3 src/run_analyses.py --ds_name chcaa/smk_canon_paintings --plot --classification
 
 deactivate
